78d17f904cfce83d7748eb44987c033121a4a2b3
Jmin0613 <jumin1356@gmail.com>
2026-05-18 14:59:15 +0900

    first commit

---
+++ src/main/java/com/example/DaejeonYeojido/DaejeonYeojidoApplication.java
@@ -0,0 +1,13 @@
+package com.example.DaejeonYeojido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaejeonYeojidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaejeonYeojidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DaejeonYeojido/DaejeonYeojidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DaejeonYeojido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaejeonYeojidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
